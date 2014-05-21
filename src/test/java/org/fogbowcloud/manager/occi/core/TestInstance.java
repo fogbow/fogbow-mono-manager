325fe8d9c4a9dfbe78340d5a2e6faf120f1a424f
fgan1 <fgan1@hotmail.com>
2014-05-21 12:52:22 -0300

    Refatoring TestInstance.

--- src/test/java/org/fogbowcloud/manager/occi/core/TestInstance.java
+++ src/test/java/org/fogbowcloud/manager/occi/core/TestInstance.java
@@ -1,10 +1,41 @@
 package org.fogbowcloud.manager.occi.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.fogbowcloud.manager.occi.instance.Instance;
+import org.fogbowcloud.manager.occi.instance.Instance.Link;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestInstance {
+
+	private List<Resource> resources = new ArrayList<Resource>();
+	private Map<String, String> attributes = new HashMap<String, String>();
+	private Link link;
+
+	@Before
+	public void setUp() {
+		Category category = new Category("compute", "http://compute", "kind");
+		Resource resource = new Resource(category, new ArrayList<String>(),
+				new ArrayList<String>(), "location", "title", "rel");
+		List<String> actionsAndSupportedAttributes = new ArrayList<String>();
+		actionsAndSupportedAttributes.add("value1");
+		actionsAndSupportedAttributes.add("value2");
+		Resource resource2 = new Resource(category, new ArrayList<String>(),
+				new ArrayList<String>(), "location", "title", "rel");
+		resources.add(resource);
+		resources.add(resource2);
+
+		attributes.put("org.openstack.compute.console.vnc", "N/A");
+		attributes.put("occi.compute.architecture", "x86");
+		attributes.put("occi.compute.speed", "0.0");
+
+		link = new Link("</network/admin>", attributes);
+	}
 
 	@Test
 	public void testParseIdOneValue() {
@@ -16,70 +47,52 @@ public class TestInstance {
 
 	@Test
 	public void testParseDetails() {
-		String textResponse = "Category: compute; scheme=\"http://schemas.ogf.org/occi/infras"
-				+ "tructure#\"; class=\"kind\"; "
-				+ "title=\"Compute Resource\"; rel=\"http://schemas.ogf.org/occi/core#resource\"; "
-				+ "location=\"http://localhost:8787/compute/\"; "
-				+ "attributes=\"occi.compute.architecture occi.compute.state{immutable} "
-				+ "occi.compute.speed occi.compute.memory occi.compute.cores occi.compute.hostname\"; "
-				+ "actions=\"http://schemas.ogf.org/occi/infrastructure/compute/action#start http://sc"
-				+ "hemas.ogf.org/occi/infrastructure/compute/action#stop "
-				+ "http://schemas.ogf.org/occi/infrastructure/compute/action#restart "
-				+ "http://schemas.ogf.org/occi/infrastructure/compute/action#suspend\"\n"
-				+ "Link: </network/admin>; rel=\"http://schemas.ogf.org/occi/infrastructure#network\"; "
-				+ "self=\"/network/interface/d7fe049b-71a3-4a91-b74b-bd191a79d423\"; "
-				+ "category=\"http://schemas.ogf.org/occi/infrastructure#networkinterface "
-				+ "http://schemas.ogf.org/occi/infrastructure/networkinterface#ipnetworkinterface\"; "
-				+ "occi.networkinterface.gateway=\"10.0.0.1\"; "
-				+ "occi.networkinterface.mac=\"fa:16:3e:5b:9d:4d\";"
-				+ " occi.networkinterface.interface=\"et"
-				+ "h0\"; occi.networkinterface.state=\"active\";"
-				+ " occi.networkinterface.allocation=\"stat"
-				+ "ic\"; occi.networkinterface.address=\"0.0.0.0\";"
-				+ " occi.core.source=\"/compute/c14902"
-				+ "07-6be7-4303-a41c-f8a4b1b9c25d\"; occi.core.target=\"/network/admin\";"
-				+ " occi.core.id="
-				+ "\"/network/interface/d7fe049b-71a3-4a91-b74b-bd191a79d423\"\n"
-				+ "X-OCCI-Attribute: org.openstack.compute.console.vnc=\"N/A\"\n"
-				+ "X-OCCI-Attribute: occi.compute.architecture=\"x86\"\n"
-				+ "X-OCCI-Attribute: occi.compute.state=\"inactive\"\n"
-				+ "X-OCCI-Attribute: occi.compute.speed=\"0.0\"\n"
-				+ "X-OCCI-Attribute: org.openstack.compute.state=\"errorz\"\n"
-				+ "X-OCCI-Attribute: occi.compute.memory=\"0.125\"\n"
-				+ "X-OCCI-Attribute: occi.compute.cores=\"1\"\n"
-				+ "X-OCCI-Attribute: occi.compute.hostname=\"server-c1490207-6be7-4303-a41c-f"
-				+ "8a4b1b9c25d\"\n"
-				+ "X-OCCI-Attribute: occi.core.id=\"c1490207-6be7-4303-a41c-f8a4b1b9c25d\"";
+		String textResponse = getFormatedResources();
+		textResponse += "\n";
+		textResponse += link.toOCCIMessageFormatLink() + "\n";
+		textResponse += getFormatedAttributes();
 		Instance instance = Instance.parseInstance("id", textResponse);
 
 		Assert.assertEquals(textResponse, instance.toOCCIMassageFormatDetails());
 	}
-	
+
 	@Test
 	public void testParseDetailsOnlyAttributes() {
-		String textResponse = ""
-				+ "X-OCCI-Attribute: org.openstack.compute.console.vnc=\"N/A\"\n"
-				+ "X-OCCI-Attribute: occi.core.id=\"c1490207-6be7-4303-a41c-f8a4b1b9c25d\"";
+		String textResponse = getFormatedAttributes();
 		Instance instance = Instance.parseInstance("id", textResponse);
 
 		Assert.assertEquals(textResponse, instance.toOCCIMassageFormatDetails());
-	}	
-	
+	}
+
 	@Test
 	public void testParseDetailsOnlyCategory() {
-		String textResponse = "Category: compute; scheme=\"http://schemas.ogf.org/occi/infras"
-				+ "tructure#\"; class=\"kind\"; "
-				+ "title=\"Compute Resource\"; rel=\"http://schemas.ogf.org/occi/core#resource\"; "
-				+ "location=\"http://localhost:8787/compute/\"; "
-				+ "attributes=\"occi.compute.architecture occi.compute.state{immutable} "
-				+ "occi.compute.speed occi.compute.memory occi.compute.cores"
-				+ " occi.compute.hostname\"; "
-				+ "actions=\"http://schemas.ogf.org/occi/infrastructure/compute/action#start"
-				+ " http://schemas.ogf.org/occi/infrastructure/compute/action#stop "
-				+ "http://schemas.ogf.org/occi/infrastructure/compute/action#restart "
-				+ "http://schemas.ogf.org/occi/infrastructure/compute/action#suspend\"";
+		String textResponse = getFormatedResources();
 		Instance instance = Instance.parseInstance("id", textResponse);
 
 		Assert.assertEquals(textResponse, instance.toOCCIMassageFormatDetails());
+	}
+
+	@Test
+	public void addAttributes() {
+		Instance instance = new Instance("");
+		instance.addAttribute("key1", "value1");
+		instance.addAttribute("key2", "value2");
+		Assert.assertEquals(2, instance.getAttributes().size());
+	}
+
+	private String getFormatedResources() {
+		String textResponse = "";
+		for (Resource resource : resources) {
+			textResponse += "Category: " + resource.toHeader() + "\n";
+		}
+		return textResponse = textResponse.trim();
+	}
+
+	private String getFormatedAttributes() {
+		String textResponse = "";
+		for (String key : attributes.keySet()) {
+			textResponse += "X-OCCI-Attribute: " + key + "=\"" + attributes.get(key) + "\"\n";
+		}
+		return textResponse = textResponse.trim();
 	}
 }
