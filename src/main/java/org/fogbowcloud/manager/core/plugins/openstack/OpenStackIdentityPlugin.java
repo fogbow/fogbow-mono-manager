4e24564f6fd46705f49ca883f72886a953bb7c49
giovannifs <giovanni@lsd.ufcg.edu.br>
2014-05-29 17:20:36 -0300

    Checking if tenant properties exist on JSONObject on OpenStackIdentityPlugin

--- src/main/java/org/fogbowcloud/manager/core/plugins/openstack/OpenStackIdentityPlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/openstack/OpenStackIdentityPlugin.java
@@ -152,16 +152,22 @@ public class OpenStackIdentityPlugin implements IdentityPlugin {
 			JSONObject tokenKeyStone = root.getJSONObject(ACCESS_KEYSTONE).getJSONObject(
 					TOKEN_KEYSTONE);
 			String token = tokenKeyStone.getString(ID_KEYSTONE);			
-//			String tenantId = tokenKeyStone.optJSONObject(TENANT_KEYSTONE).getString(ID_KEYSTONE);
-//			String tenantName = tokenKeyStone.getJSONObject(TENANT_KEYSTONE).getString(
-//					NAME_KEYSTONE);
+			String tenantId = "";
+			String tenantName = "";
+			Map<String, String> tokenAtt = new HashMap<String, String>();
+			try {
+				tenantId = tokenKeyStone.getJSONObject(TENANT_KEYSTONE).getString(ID_KEYSTONE);
+				tenantName = tokenKeyStone.getJSONObject(TENANT_KEYSTONE).getString(
+					NAME_KEYSTONE);
+				tokenAtt.put(TENANT_ID_KEY, tenantId);
+				tokenAtt.put(TENANT_NAME_KEY, tenantName);
+			} catch (JSONException e) {
+				LOGGER.debug("There are not tenant properties on json.");
+			}
 			String expirationDateToken = tokenKeyStone.getString(EXPIRES_KEYSTONE);
 			String user = root.getJSONObject(ACCESS_KEYSTONE).getJSONObject(USER_KEYSTONE)
 					.getString(NAME_KEYSTONE);
-
-			Map<String, String> tokenAtt = new HashMap<String, String>();
-//			tokenAtt.put(TENANT_ID_KEY, tenantId);
-//			tokenAtt.put(TENANT_NAME_KEY, tenantName);
+		
 			LOGGER.debug("json token: " + token);
 			LOGGER.debug("json user: " + user);
 			LOGGER.debug("json expirationDate: " + expirationDateToken);
