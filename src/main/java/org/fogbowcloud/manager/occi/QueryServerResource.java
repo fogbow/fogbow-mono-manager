8b1ff74e826a8d9d442c365e2ed8f1bd6d7d4d9c
giovannifs <giovanni@lsd.ufcg.edu.br>
2014-07-10 12:11:32 -0300

    Adding log for query interface

--- src/main/java/org/fogbowcloud/manager/occi/QueryServerResource.java
+++ src/main/java/org/fogbowcloud/manager/occi/QueryServerResource.java
@@ -3,6 +3,8 @@ package org.fogbowcloud.manager.occi;
 import java.util.List;
 
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
+import org.fogbowcloud.manager.core.ManagerController;
 import org.fogbowcloud.manager.occi.core.HeaderUtils;
 import org.fogbowcloud.manager.occi.core.OCCIHeaders;
 import org.fogbowcloud.manager.occi.core.Resource;
@@ -16,11 +18,16 @@ import org.restlet.util.Series;
 
 public class QueryServerResource extends ServerResource {
 
+	private static final Logger LOGGER = Logger.getLogger(QueryServerResource.class);
+
 	@Get
 	public String fetch() {
+		LOGGER.debug("Executing the query interface fetch method");
 		if (getRequest().getMethod().equals(Method.HEAD)){
+			LOGGER.debug("It is a HEAD method request");
 			HttpRequest req = (HttpRequest) getRequest();
 			String token = req.getHeaders().getValues(OCCIHeaders.X_AUTH_TOKEN);
+			LOGGER.debug("Auth Token = " + token);
 			if (token == null || token.equals("")) {
 				Series<Header> responseHeaders = (Series<Header>) getResponse().getAttributes().get("org.restlet.http.headers");
 				if (responseHeaders == null) {
@@ -33,12 +40,14 @@ public class QueryServerResource extends ServerResource {
 			}
 			return "";
 		} else {
+			LOGGER.debug("It is a GET method request");
 			OCCIApplication application = (OCCIApplication) getApplication();
 			HttpRequest req = (HttpRequest) getRequest();
 			HeaderUtils.checkOCCIContentType(req.getHeaders());
 			String authToken = HeaderUtils.getAuthToken(req.getHeaders(), getResponse());
-			
+			LOGGER.debug("Auth Token = " + authToken);			
 			List<Resource> allResources = application.getAllResources(authToken);
+			LOGGER.debug("All Resources = " + allResources);
 			return generateResponse(allResources);
 		}
 		
