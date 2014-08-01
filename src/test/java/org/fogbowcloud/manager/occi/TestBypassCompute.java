24951885e4d7dc716448624db2732d4a094d1e38
giovannifs <giovanni@lsd.ufcg.edu.br>
2014-08-01 10:05:11 -0300

    Adding test class for bypass compute

---
+++ src/test/java/org/fogbowcloud/manager/occi/TestBypassCompute.java
@@ -0,0 +1,289 @@
+package org.fogbowcloud.manager.occi;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.fogbowcloud.manager.core.ConfigurationConstants;
+import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
+import org.fogbowcloud.manager.core.plugins.openstack.OpenStackComputePlugin;
+import org.fogbowcloud.manager.core.util.DefaultDataTestHelper;
+import org.fogbowcloud.manager.occi.core.Category;
+import org.fogbowcloud.manager.occi.core.HeaderUtils;
+import org.fogbowcloud.manager.occi.core.OCCIHeaders;
+import org.fogbowcloud.manager.occi.core.ResponseConstants;
+import org.fogbowcloud.manager.occi.core.Token;
+import org.fogbowcloud.manager.occi.request.Request;
+import org.fogbowcloud.manager.occi.request.RequestConstants;
+import org.fogbowcloud.manager.occi.util.ComputeApplication;
+import org.fogbowcloud.manager.occi.util.OCCITestHelper;
+import org.fogbowcloud.manager.occi.util.PluginHelper;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class TestBypassCompute {
+
+	private static final int LITTLE_SCHEDULE_TIME = 100;
+	private static final String FIRST_INSTANCE_ID = "first-instance";
+	private static final String SECOND_INSTANCE_ID = "second-instance";
+	private static final String THIRD_INSTANCE_ID = "third-instance";
+	private static final String FOURTH_INSTANCE_ID = "fourth-instance";
+	private static final String FIFTH_INSTANCE_ID = "fifth-instance";
+	private PluginHelper pluginHelper = new PluginHelper();
+	private OCCITestHelper helper;
+	private OpenStackComputePlugin computePlugin;
+	private IdentityPlugin identityPlugin;
+	
+	@Before
+	public void setup() throws Exception{
+		setup(null);
+	}
+	
+	private void setup(List<Request> requests) throws Exception {
+		if (requests == null) {
+			requests = new ArrayList<Request>();
+		}
+		
+		Properties properties = new Properties();
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_URL_KEY, PluginHelper.COMPUTE_OCCI_URL);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_INSTANCE_SCHEME_KEY, ComputeApplication.INSTANCE_SCHEME);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_OS_SCHEME_KEY, ComputeApplication.OS_SCHEME);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_RESOURCE_SCHEME_KEY, ComputeApplication.RESOURCE_SCHEME);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_FLAVOR_SMALL_KEY, ComputeApplication.SMALL_FLAVOR_TERM);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_FLAVOR_MEDIUM_KEY, ComputeApplication.MEDIUM_FLAVOR_TERM);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_FLAVOR_LARGE_KEY, ComputeApplication.MEDIUM_FLAVOR_TERM);
+		properties.put(ConfigurationConstants.COMPUTE_OCCI_IMAGE_PREFIX + PluginHelper.LINUX_X86_TERM, PluginHelper.CIRROS_IMAGE_TERM);
+		properties.put(ConfigurationConstants.SCHEDULER_PERIOD_KEY, LITTLE_SCHEDULE_TIME);
+		
+		computePlugin = new OpenStackComputePlugin(properties);
+		
+		identityPlugin = Mockito.mock(IdentityPlugin.class);
+		Mockito.when(identityPlugin.getToken(PluginHelper.ACCESS_ID)).thenReturn(
+				new Token(PluginHelper.ACCESS_ID, PluginHelper.USERNAME, DefaultDataTestHelper.TOKEN_FUTURE_EXPIRATION,
+						new HashMap<String, String>()));
+		
+		// three first generated instance ids
+		List<String> expectedInstanceIds = new ArrayList<String>();
+		expectedInstanceIds.add(FIRST_INSTANCE_ID);
+		expectedInstanceIds.add(SECOND_INSTANCE_ID);
+		expectedInstanceIds.add(THIRD_INSTANCE_ID);
+		expectedInstanceIds.add(FOURTH_INSTANCE_ID);
+		expectedInstanceIds.add(FIFTH_INSTANCE_ID);
+		
+		//initializing fake Cloud Compute Application
+		pluginHelper.initializeComputeComponent(expectedInstanceIds);
+		
+		//initializing fogbow OCCI Application
+		helper = new OCCITestHelper();
+		helper.initializeComponentCompute(computePlugin, identityPlugin, requests);
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		pluginHelper.disconnectComponent();
+		helper.stopComponent();
+	}
+	
+	
+	@Test
+	public void testBypassPostComputeOK() throws URISyntaxException, HttpException, IOException {
+		//post compute through fogbow endpoint
+		HttpPost httpPost = new HttpPost(OCCITestHelper.URI_FOGBOW_COMPUTE);
+		httpPost.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		httpPost.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		httpPost.addHeader(OCCIHeaders.CATEGORY, new Category(PluginHelper.LINUX_X86_TERM,
+				OpenStackComputePlugin.getOSScheme(), RequestConstants.MIXIN_CLASS).toHeader());
+		
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpPost);
+		
+		Assert.assertEquals(PluginHelper.COMPUTE_OCCI_URL + OpenStackComputePlugin.COMPUTE_ENDPOINT
+				+ FIRST_INSTANCE_ID, response.getFirstHeader(HeaderUtils.normalize("Location")).getValue());		
+		Assert.assertTrue(response.getEntity().getContentType().getValue().startsWith(OCCIHeaders.TEXT_PLAIN_CONTENT_TYPE));
+		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+		String message = EntityUtils.toString(response.getEntity(),
+				String.valueOf(Charsets.UTF_8));
+		Assert.assertEquals(ResponseConstants.OK, message);
+	}
+	
+	@Test
+	public void testBypassPostComputeWithWrongMediaTypeTextPlain() throws URISyntaxException, HttpException, IOException {
+		//post compute through fogbow endpoint
+		HttpPost httpPost = new HttpPost(OCCITestHelper.URI_FOGBOW_COMPUTE);
+		httpPost.addHeader(OCCIHeaders.CONTENT_TYPE, "invalid-type");
+		httpPost.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		httpPost.addHeader(OCCIHeaders.CATEGORY, new Category(PluginHelper.LINUX_X86_TERM,
+				OpenStackComputePlugin.getOSScheme(), RequestConstants.MIXIN_CLASS).toHeader());
+		
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpPost);
+
+		Assert.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusLine().getStatusCode());
+	}
+	
+	@Test
+	public void testBypassPostComputeWithoutMediaType() throws URISyntaxException, HttpException, IOException {
+		//post compute through fogbow endpoint
+		HttpPost httpPost = new HttpPost(OCCITestHelper.URI_FOGBOW_COMPUTE);
+		httpPost.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		httpPost.addHeader(OCCIHeaders.CATEGORY, new Category(PluginHelper.LINUX_X86_TERM,
+				OpenStackComputePlugin.getOSScheme(), RequestConstants.MIXIN_CLASS).toHeader());
+		
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpPost);
+
+		Assert.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getStatusLine().getStatusCode());
+	}
+	
+	@Test
+	public void testBypassGetComputeOK() throws URISyntaxException, HttpException, IOException {
+		//adding instances directly on compute endpoint
+		List<Category> categories = new ArrayList<Category>();		
+		categories.add(new Category(PluginHelper.LINUX_X86_TERM,
+				OpenStackComputePlugin.getOSScheme(), RequestConstants.MIXIN_CLASS));
+		Assert.assertEquals(FIRST_INSTANCE_ID, computePlugin.requestInstance(
+				PluginHelper.ACCESS_ID, categories, new HashMap<String, String>()));
+		Assert.assertEquals(SECOND_INSTANCE_ID, computePlugin.requestInstance(
+				PluginHelper.ACCESS_ID, categories, new HashMap<String, String>()));
+		Assert.assertEquals(THIRD_INSTANCE_ID, computePlugin.requestInstance(
+				PluginHelper.ACCESS_ID, categories, new HashMap<String, String>()));
+
+		//checking if machines were added
+		Assert.assertEquals(3, computePlugin.getInstances(PluginHelper.ACCESS_ID).size());
+		
+		//getting instances through fogbow endpoint		
+		HttpGet httpGet = new HttpGet(OCCITestHelper.URI_FOGBOW_COMPUTE);
+		httpGet.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		httpGet.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpGet);
+	
+		//checking instances are OK
+		Assert.assertEquals(3, OCCITestHelper.getRequestIds(response).size());
+		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+	}
+	
+	@Test
+	public void testGetBypassGetComputeOK() throws Exception {
+		//adding instance through fogbow endpoint
+		HttpPost post = new HttpPost(OCCITestHelper.URI_FOGBOW_REQUEST);
+		post.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		post.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		post.addHeader(OCCIHeaders.CATEGORY, new Category(RequestConstants.TERM,
+				RequestConstants.SCHEME, RequestConstants.KIND_CLASS).toHeader());
+		post.addHeader(OCCIHeaders.CATEGORY, new Category(PluginHelper.LINUX_X86_TERM,
+				RequestConstants.TEMPLATE_OS_SCHEME, RequestConstants.MIXIN_CLASS).toHeader());
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(post);
+		List<String> requestIDs = OCCITestHelper.getRequestIds(response);
+
+		Assert.assertEquals(1, requestIDs.size());
+		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+		
+		Thread.sleep(LITTLE_SCHEDULE_TIME + 15);
+		
+		//adding instances directly on compute endpoint
+		List<Category> categories = new ArrayList<Category>();		
+		categories.add(new Category(PluginHelper.LINUX_X86_TERM,
+				OpenStackComputePlugin.getOSScheme(), RequestConstants.MIXIN_CLASS));
+		Assert.assertEquals(SECOND_INSTANCE_ID, computePlugin.requestInstance(
+				PluginHelper.ACCESS_ID, categories, new HashMap<String, String>()));
+		Assert.assertEquals(THIRD_INSTANCE_ID, computePlugin.requestInstance(
+				PluginHelper.ACCESS_ID, categories, new HashMap<String, String>()));
+		
+		//checking if machines were added
+		Assert.assertEquals(3, computePlugin.getInstances(PluginHelper.ACCESS_ID).size());
+		
+		//getting instances through fogbow endpoint		
+		HttpGet httpGet = new HttpGet(OCCITestHelper.URI_FOGBOW_COMPUTE);
+		httpGet.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		httpGet.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		client = new DefaultHttpClient();
+		response = client.execute(httpGet);
+	
+		//checking instances are OK
+		Assert.assertEquals(3, OCCITestHelper.getRequestIds(response).size());
+		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+	}
+
+	@Test
+	public void testBypassGetSpecificComputeOK() throws URISyntaxException, HttpException, IOException {
+		//adding instances directly on compute endpoint
+		List<Category> categories = new ArrayList<Category>();		
+		categories.add(new Category(PluginHelper.LINUX_X86_TERM,
+				OpenStackComputePlugin.getOSScheme(), RequestConstants.MIXIN_CLASS));
+		Assert.assertEquals(FIRST_INSTANCE_ID, computePlugin.requestInstance(
+				PluginHelper.ACCESS_ID, categories, new HashMap<String, String>()));
+		
+		//checking if machine was added
+		Assert.assertEquals(1, computePlugin.getInstances(PluginHelper.ACCESS_ID).size());
+		
+		//getting specific instance through fogbow endpoint		
+		HttpGet httpGet = new HttpGet(OCCITestHelper.URI_FOGBOW_COMPUTE + FIRST_INSTANCE_ID);
+		httpGet.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		httpGet.addHeader(OCCIHeaders.X_AUTH_TOKEN, PluginHelper.ACCESS_ID);
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpGet);
+	
+		//checking instance details
+		Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());		
+		String instanceDetails = EntityUtils.toString(response.getEntity());
+		Assert.assertEquals(FIRST_INSTANCE_ID,
+				pluginHelper.getAttValueFromInstanceDetails(instanceDetails, ComputeApplication.ID_CORE_ATTRIBUTE_OCCI));
+		Assert.assertEquals(1, Integer.parseInt(pluginHelper.getAttValueFromInstanceDetails(instanceDetails,
+				ComputeApplication.CORE_ATTRIBUTE_OCCI)));
+		Assert.assertEquals(2, Integer.parseInt(pluginHelper.getAttValueFromInstanceDetails(instanceDetails,
+				ComputeApplication.MEMORY_ATTRIBUTE_OCCI)));
+		Assert.assertEquals(64, Integer.parseInt(pluginHelper.getAttValueFromInstanceDetails(instanceDetails,
+				ComputeApplication.ARCHITECTURE_ATTRIBUTE_OCCI)));
+		Assert.assertEquals("server-" + FIRST_INSTANCE_ID,
+				pluginHelper.getAttValueFromInstanceDetails(instanceDetails, ComputeApplication.HOSTNAME_ATTRIBUTE_OCCI));
+	}
+	
+	@Test
+	public void testBypassGetComputeWithEmptyToken() throws URISyntaxException, HttpException, IOException {
+		//getting instances through fogbow endpoint without auth-token
+		HttpGet httpGet = new HttpGet(OCCITestHelper.URI_FOGBOW_COMPUTE);
+		httpGet.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		httpGet.addHeader(OCCIHeaders.X_AUTH_TOKEN, "");
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpGet);
+	
+		//checking unauthorized status
+		Assert.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
+		Assert.assertEquals("Keystone uri='http://localhost:5000/'",
+				response.getFirstHeader(HeaderUtils.WWW_AUTHENTICATE).getValue());
+		Assert.assertTrue(response.getFirstHeader(OCCIHeaders.CONTENT_TYPE).getValue()
+				.startsWith(OCCIHeaders.TEXT_PLAIN_CONTENT_TYPE));
+		Assert.assertEquals(ResponseConstants.UNAUTHORIZED,
+				EntityUtils.toString(response.getEntity()));
+	}
+	
+	@Test
+	public void testGetSpecificInstanceNotFound() throws Exception {
+		//getting not existing instance through fogbow endpoint
+		HttpGet httpGet = new HttpGet(OCCITestHelper.URI_FOGBOW_COMPUTE + "wrong");
+		httpGet.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		httpGet.addHeader(OCCIHeaders.X_AUTH_TOKEN, OCCITestHelper.ACCESS_TOKEN);
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(httpGet);
+
+		Assert.assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusLine().getStatusCode());
+	}
+}
