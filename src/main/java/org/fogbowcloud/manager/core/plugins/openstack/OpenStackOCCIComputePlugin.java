9f1c334b9fddc6d9adb039bf74bfbdd0e0f5f4fa
fgan1 <fgan1@hotmail.com>
2015-01-22 11:29:10 -0300

    Refatoring OpenStackOCCIComputePlugin.

--- src/main/java/org/fogbowcloud/manager/core/plugins/openstack/OpenStackOCCIComputePlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/openstack/OpenStackOCCIComputePlugin.java
@@ -11,7 +11,6 @@ import java.util.Set;
 
 import org.apache.http.Header;
 import org.apache.http.message.BasicHeader;
-import org.fogbowcloud.manager.core.model.Flavor;
 import org.fogbowcloud.manager.core.model.ResourcesInfo;
 import org.fogbowcloud.manager.core.plugins.occi.OCCIComputePlugin;
 import org.fogbowcloud.manager.occi.core.Category;
@@ -22,8 +21,6 @@ import org.fogbowcloud.manager.occi.core.ResponseConstants;
 import org.fogbowcloud.manager.occi.core.Token;
 import org.fogbowcloud.manager.occi.request.RequestAttribute;
 import org.fogbowcloud.manager.occi.request.RequestConstants;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.restlet.Client;
 import org.restlet.Request;
 import org.restlet.data.ClientInfo;
@@ -39,18 +36,11 @@ public class OpenStackOCCIComputePlugin extends OCCIComputePlugin{
 	private static final String DATA_PUBLIC_KEY_ATTRIBUTE = "org.openstack.credentials.publickey.data";
 	private static final String NAME_PUBLIC_KEY_DEFAULT = "fogbow_keypair";
 	
-	private static final String ABSOLUTE = "absolute";
-	private static final String LIMITS = "limits";
-	private String computeV2APIEndpoint;
-	private static final String TENANT_ID = "tenantId";
-	private final String COMPUTE_V2_API_ENDPOINT = "/v2/";
 	private OpenStackNovaV2ComputePlugin openStackNovaV2ComputePlugin;
 	
 	public OpenStackOCCIComputePlugin(Properties properties) {
 		super(properties);
 		openStackNovaV2ComputePlugin = new OpenStackNovaV2ComputePlugin(properties);
-		this.computeV2APIEndpoint = properties.getProperty("compute_openstack_v2api_url")
-				+ COMPUTE_V2_API_ENDPOINT;
 		super.fogTermToCategory.put(RequestConstants.PUBLIC_KEY_TERM, new Category(
 				PUBLIC_KEY_TERM, PUBLIC_KEY_SCHEME, RequestConstants.MIXIN_CLASS));
 	}
@@ -161,26 +151,7 @@ public class OpenStackOCCIComputePlugin extends OCCIComputePlugin{
 	
 	@Override
 	public ResourcesInfo getResourcesInfo(Token token) {
-		String responseStr = doRequest(
-				"get",
-				computeV2APIEndpoint
-						+ token.getAttributes().get(TENANT_ID)
-		+ "/limits", token.getAccessId()).getResponseString();
-
-		String maxCpu = getAttFromJson(OpenStackConfigurationConstants.MAX_TOTAL_CORES_ATT,
-				responseStr);
-		String cpuInUse = getAttFromJson(OpenStackConfigurationConstants.TOTAL_CORES_USED_ATT,
-				responseStr);
-		String maxMem = getAttFromJson(OpenStackConfigurationConstants.MAX_TOTAL_RAM_SIZE_ATT,
-				responseStr);
-		String memInUse = getAttFromJson(OpenStackConfigurationConstants.TOTAL_RAM_USED_ATT,
-				responseStr);
-
-		int cpuIdle = Integer.parseInt(maxCpu) - Integer.parseInt(cpuInUse);
-		int memIdle = Integer.parseInt(maxMem) - Integer.parseInt(memInUse);
-
-		return new ResourcesInfo(String.valueOf(cpuIdle), cpuInUse, String.valueOf(memIdle),
-				memInUse, getFlavors(cpuIdle, memIdle), null);
+		return openStackNovaV2ComputePlugin.getResourcesInfo(token);
 	}	
 
 	private void convertRequestToOcci(Request request, Series<org.restlet.engine.header.Header> requestHeaders) {
@@ -212,30 +183,6 @@ public class OpenStackOCCIComputePlugin extends OCCIComputePlugin{
 					attribute.trim().replace("\n", ",")));
 			request.setEntity("", MediaType.TEXT_PLAIN);			
 		} catch (Exception e) {}
-	}
-	
-	private String getAttFromJson(String attName, String responseStr) {
-		try {
-			JSONObject root = new JSONObject(responseStr);
-			return root.getJSONObject(LIMITS).getJSONObject(ABSOLUTE).getString(attName).toString();
-		} catch (JSONException e) {
-			return null;
-		}
-	}
-	
-	private List<Flavor> getFlavors(int cpuIdle, int memIdle) {
-		List<Flavor> flavors = new ArrayList<Flavor>();
-		// flavors
-		int capacity = Math.min(cpuIdle / 1, memIdle / 2048);
-		Flavor smallFlavor = new Flavor(RequestConstants.SMALL_TERM, "1", "2048", capacity);
-		capacity = Math.min(cpuIdle / 2, memIdle / 4096);
-		Flavor mediumFlavor = new Flavor(RequestConstants.MEDIUM_TERM, "2", "4096", capacity);
-		capacity = Math.min(cpuIdle / 4, memIdle / 8192);
-		Flavor largeFlavor = new Flavor(RequestConstants.LARGE_TERM, "4", "8192", capacity);
-		flavors.add(smallFlavor);
-		flavors.add(mediumFlavor);
-		flavors.add(largeFlavor);
-		return flavors;
 	}
 	
 	public void uploadImage(Token token, String imagePath, String imageName) {
