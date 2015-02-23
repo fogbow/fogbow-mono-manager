1bfe7b2037917a73dd1e66cb65751e9d0e4ce042
giovannifs <giovanni@lsd.ufcg.edu.br>
2015-02-23 18:40:35 -0300

    Putting the same value for Federation and Local authentication token

--- src/main/java/org/fogbowcloud/manager/occi/core/OCCIHeaders.java
+++ src/main/java/org/fogbowcloud/manager/occi/core/OCCIHeaders.java
@@ -5,8 +5,10 @@ public class OCCIHeaders {
 
 	// header constants
 	public static final String CONTENT_TYPE = "Content-Type";
-	public static final String X_FEDERATION_AUTH_TOKEN = "X-Federation-Auth-Token";
-	public static final String X_LOCAL_AUTH_TOKEN = "X-Local-Auth-Token";
+//	public static final String X_FEDERATION_AUTH_TOKEN = "X-Federation-Auth-Token";
+//	public static final String X_LOCAL_AUTH_TOKEN = "X-Local-Auth-Token";
+	public static final String X_FEDERATION_AUTH_TOKEN = "X-Auth-Token";
+	public static final String X_LOCAL_AUTH_TOKEN = "X-Auth-Token";
 	public static final String X_SUBJECT_TOKEN = "X-Subject-Token";
 	public static final String X_OCCI_ATTRIBUTE = "X-OCCI-Attribute";
 	public static final String ACCEPT = "Accept";
