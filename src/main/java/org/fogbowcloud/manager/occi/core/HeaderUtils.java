ae86724be88d74fb3b892363ad9ce5bb3874e6ae
giovannifs <giovanni@lsd.ufcg.edu.br>
2015-02-24 09:19:40 -0300

    Adding log

--- src/main/java/org/fogbowcloud/manager/occi/core/HeaderUtils.java
+++ src/main/java/org/fogbowcloud/manager/occi/core/HeaderUtils.java
@@ -45,6 +45,7 @@ public class HeaderUtils {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static String getAuthToken(Series<Header> headers, Response response, String authenticationURI) {
 		String token = headers.getValues(OCCIHeaders.X_FEDERATION_AUTH_TOKEN);
+		LOGGER.debug("auth-token=" + token);
 		if (token == null || token.equals("")) {
 			if (authenticationURI != null) {
 				if (response != null) {
@@ -60,6 +61,7 @@ public class HeaderUtils {
 				throw new OCCIException(ErrorType.UNAUTHORIZED, ResponseConstants.UNAUTHORIZED);
 			} else {
 				String authorizationHeader = headers.getValues(HeaderUtils.AUTHORIZATION);
+				LOGGER.debug("authorization-header=" + authorizationHeader);
 				if (authorizationHeader == null) {
 					throw new OCCIException(ErrorType.UNAUTHORIZED, ResponseConstants.UNAUTHORIZED);
 				}
