30ba255cfa49241ef2dca17c76a06177a0cc2267
marcosnobrega <marcosnobregajr@gmail.com>
2015-05-13 16:53:18 -0300

    Thanks lord! Requirements working with default values!

--- src/main/java/org/fogbowcloud/manager/core/plugins/compute/opennebula/OpenNebulaComputePlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/compute/opennebula/OpenNebulaComputePlugin.java
@@ -707,7 +707,7 @@ public class OpenNebulaComputePlugin implements ComputePlugin {
 			String term, int minimumValue) {
 		String valueStr = RequirementsHelper
 				.getSmallestValueForAttribute(requirements, term);
-		return String.valueOf(Math.max(Double.valueOf(valueStr), minimumValue));
+		return String.valueOf(Math.max(Integer.valueOf(valueStr), minimumValue));
 	}
 
 	protected List<String> getTemplatesInProperties(Properties properties) {
