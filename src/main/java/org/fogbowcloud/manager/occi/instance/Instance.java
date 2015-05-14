81cecd73fcae62bbb66d03116e1432bcb8c107cf
fgan1 <fgan1@hotmail.com>
2015-05-14 15:41:02 -0300

    Adding EXTRA_PORTS_ATT constant.

--- src/main/java/org/fogbowcloud/manager/occi/instance/Instance.java
+++ src/main/java/org/fogbowcloud/manager/occi/instance/Instance.java
@@ -14,6 +14,7 @@ public class Instance {
 	public static final String PREFIX_DEFAULT_INSTANCE = "X-OCCI-Location: ";
 	public static final String SSH_PUBLIC_ADDRESS_ATT = "org.fogbowcloud.request.ssh-public-address";
 	public static final String SSH_USERNAME_ATT = "org.fogbowcloud.request.ssh-username";
+	public static final String EXTRA_PORTS_ATT = "org.fogbowcloud.request.extra-ports";
 	
 	private static final String PREFIX_DEFAULT_ATTRIBUTE = "X-OCCI-Attribute: ";
 	private static final String CATEGORY = "Category:";
