72e324db6f50be1a02232a48e536eb7cd99e0182
Abmar Barros <abmargb@gmail.com>
2015-05-19 18:35:05 -0300

    Update lvl-user-data.sh

--- examples/lvl-user-data.sh
+++ examples/lvl-user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 create-fogbow-tunnel http 80
-sed -i 's/include \/etc\/nginx\/naxsi/#include \/etc\/nginx/g' /etc/nginx/sites-enabled/default
+sed -i 's/include \/etc\/nginx\/naxsi//g' /etc/nginx/sites-enabled/default
 service nginx restart
