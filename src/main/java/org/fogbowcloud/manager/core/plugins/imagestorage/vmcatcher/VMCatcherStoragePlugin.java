820b3055bfc73143da39b7ff47ff072b8ace6e19
giovannifs <giovanni@lsd.ufcg.edu.br>
2015-08-04 14:57:45 -0300

    Making VMcatcherStoragePlugin check if image already exists in underlying cloud before getting it from VMcatcher repository.

--- src/main/java/org/fogbowcloud/manager/core/plugins/imagestorage/vmcatcher/VMCatcherStoragePlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/imagestorage/vmcatcher/VMCatcherStoragePlugin.java
@@ -75,6 +75,11 @@ public class VMCatcherStoragePlugin extends StaticImageStoragePlugin {
 			return null;
 		}
 		
+		localId = computePlugin.getImageId(token, globalId);
+		if (localId != null) {
+			return localId;
+		}
+		
 		String imageTitleTranslated = null;
 		final String pushMethod = this.props.getProperty(PROP_VMC_PUSH_METHOD);
 		if (pushMethod.equals("glancepush")) {
