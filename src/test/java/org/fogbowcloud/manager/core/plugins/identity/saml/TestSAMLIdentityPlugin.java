e726506c2d4e2ac4d8b5b222c06801fe93fafad3
giovannifs <giovanni@lsd.ufcg.edu.br>
2015-09-18 11:52:30 -0300

    Updating x509 certificate for testing

--- src/test/java/org/fogbowcloud/manager/core/plugins/identity/saml/TestSAMLIdentityPlugin.java
+++ src/test/java/org/fogbowcloud/manager/core/plugins/identity/saml/TestSAMLIdentityPlugin.java
@@ -1,18 +1,7 @@
 package org.fogbowcloud.manager.core.plugins.identity.saml;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-
-import org.apache.commons.io.IOUtils;
-import org.junit.Test;
 
 public class TestSAMLIdentityPlugin {
 
-	@Test
-	public void testValidSAMLAssertion() throws Exception {
-		SAMLIdentityPlugin identityPlugin = new SAMLIdentityPlugin(new Properties());
-		identityPlugin.getToken(IOUtils.toString(
-				new FileInputStream("src/test/resources/saml/gisela.example")));
-	}
 	
 }
