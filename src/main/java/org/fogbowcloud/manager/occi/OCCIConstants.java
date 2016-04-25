d5836dc532b29313a1d1cae71e0c7eb4f3c69c96
fgan1 <fgan1@hotmail.com>
2016-04-25 15:22:40 -0300

    Implementing OCCIConstants

---
+++ src/main/java/org/fogbowcloud/manager/occi/OCCIConstants.java
@@ -0,0 +1,12 @@
+package org.fogbowcloud.manager.occi;
+
+public class OCCIConstants {
+
+	public static final String NETWORK_VLAN = "occi.network.vlan";
+	public static final String NETWORK_LABEL = "occi.network.label";
+	public static final String NETWORK_STATE = "occi.network.state";
+	public static final String NETWORK_ADDRESS = "occi.network.address";
+	public static final String NETWORK_GATEWAY = "occi.network.gateway";
+	public static final String NETWORK_ALLOCANTION = "occi.network.allocation";
+	
+}
