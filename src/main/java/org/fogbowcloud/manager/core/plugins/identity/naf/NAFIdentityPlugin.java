31d36a90eba12e8ebf9eec864b8fce1da9e7289c
fgan1 <fgan1@hotmail.com>
2016-08-02 15:01:45 -0300

    Adding: check token generator.

--- src/main/java/org/fogbowcloud/manager/core/plugins/identity/naf/NAFIdentityPlugin.java
+++ src/main/java/org/fogbowcloud/manager/core/plugins/identity/naf/NAFIdentityPlugin.java
@@ -1,5 +1,6 @@
 package org.fogbowcloud.manager.core.plugins.identity.naf;
 
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
@@ -8,6 +9,14 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.io.Charsets;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.bouncycastle.util.encoders.Base64;
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
@@ -21,20 +30,43 @@ import org.json.JSONObject;
 
 public class NAFIdentityPlugin implements IdentityPlugin {
 
+	protected static final String TOKEN_URL_OPERATION = "/token/";
+	protected static final String METHOD_GET_VALIDITY_CHECK = "?method=validityCheck";
+	protected static final String TYPE = "type";
+	protected static final String VALID_RESPONSE_TOKEN_GENERATOR = "Valid";
+	protected static final String PASSWORD_USER_TOKEN_GENERATOR = "password_user_token_generator";
+	protected static final String NAME_USER_TOKEN_GENERATOR = "name_user_token_generator";
+	protected static final String ENDPOINT_TOKEN_GENERATOR = "endpoint_token_generator";
+	protected static final String DEFAULT_TYPE_TOKEN_GENERATOR = "token_generator";
 	protected static final String SAML_ATTRIBUTES_JSONOBJECT = "saml_attributes";
-	protected static final String TOKEN_CTIME_JSONOBJECT = "token_ctime";
+	protected static final String TOKEN_ETIME_JSONOBJECT = "token_etime";
+	protected static final String PASSWORD = "password";
 	protected static final String NAME = "name";
 	
 	private static final Logger LOGGER = Logger.getLogger(NAFIdentityPlugin.class);
-	protected static final String NAF_PUBLIC_KEY = "naf_identity_plublic_key";
+	protected static final String NAF_PUBLIC_KEY = "naf_identity_public_key";
 	protected static final String STRING_SEPARATOR = "!#!";
 	
 	private Properties properties;
+	private HttpClient client;
 	
 	public NAFIdentityPlugin(Properties properties) {
 		this.properties = properties;
+		initClient();
 	}
 
+	private void initClient() {
+		client = HttpClients.createMinimal();
+	}
+	
+	protected HttpClient getClient() {
+		return client;
+	}
+	
+	protected void setClient(HttpClient client) {
+		this.client = client;
+	}
+	
 	@Override
 	public Token createToken(Map<String, String> userCredentials) {
 		return null;
@@ -63,8 +95,8 @@ public class NAFIdentityPlugin implements IdentityPlugin {
 			AccessIdFormat accessIdFormat = new AccessIdFormat(accessIdDecoded);
 			jsonObject = new JSONObject(accessIdFormat.getMessage());
 			user = jsonObject.getString(NAME);
-			date = new Date(Long.parseLong(jsonObject.getString(TOKEN_CTIME_JSONOBJECT)));
-			attributes = toMap(jsonObject.getJSONObject(SAML_ATTRIBUTES_JSONOBJECT));
+			date = new Date(Long.parseLong(jsonObject.getString(TOKEN_ETIME_JSONOBJECT)));
+			attributes = toMap(jsonObject.optJSONObject(SAML_ATTRIBUTES_JSONOBJECT));
 		} catch (Exception e) {
 			LOGGER.error("Could not create token by accessId.", e);
 			throw new OCCIException(ErrorType.UNAUTHORIZED, ResponseConstants.UNAUTHORIZED);
@@ -78,7 +110,7 @@ public class NAFIdentityPlugin implements IdentityPlugin {
 		if (accessId == null || accessId.isEmpty()) {
 			throw new OCCIException(ErrorType.UNAUTHORIZED, ResponseConstants.UNAUTHORIZED);
 		}
-		accessId = new String(Base64.decode(accessId.getBytes()), Charset.forName("UTF-8")); 		
+		String accessIdDecoded = new String(Base64.decode(accessId.getBytes()), Charset.forName("UTF-8")); 		
 		RSAPublicKey publicKey = null;
 		try {
 			publicKey = RSAUtils.getPublicKey(properties.getProperty(NAF_PUBLIC_KEY));
@@ -87,13 +119,42 @@ public class NAFIdentityPlugin implements IdentityPlugin {
 			return false;
 		}
 		
+		AccessIdFormat accessIdFormat = null;
 		try {
-			AccessIdFormat accessIdFormat = new AccessIdFormat(accessId);
+			accessIdFormat = new AccessIdFormat(accessIdDecoded);
 			RSAUtils.verify(publicKey, accessIdFormat.getMessage(), accessIdFormat.getSignature());
 		} catch (Exception e) {			
 			LOGGER.warn("Could not verify access id.", e);
 			return false;
 		}
+		
+		try {
+			String jsonTokenSlice = accessIdFormat.getMessage();
+			String type = new JSONObject(jsonTokenSlice).optString(TYPE);
+			if (jsonTokenSlice != null && type != null && type.equals(DEFAULT_TYPE_TOKEN_GENERATOR)) {
+				if (!isValidTokenGenerator(accessIdFormat.getMessage(), URLEncoder.encode(accessId, "UTF-8"))) {
+					return false;
+				}				
+			}
+		} catch (Exception e) {
+			LOGGER.warn("Could not check if is token generator.", e);
+			return false;
+		}
+		
+		return true;
+	}
+
+	private boolean isValidTokenGenerator(String json, String accessId) throws JSONException {		
+		String endpoint = this.properties.getProperty(ENDPOINT_TOKEN_GENERATOR);
+		String username = this.properties.getProperty(NAME_USER_TOKEN_GENERATOR);
+		String password = this.properties.getProperty(PASSWORD_USER_TOKEN_GENERATOR);
+		String responseStr = doGetRequest(endpoint + TOKEN_URL_OPERATION + accessId 
+				+ METHOD_GET_VALIDITY_CHECK, username, password);
+				
+		if (!responseStr.equals(VALID_RESPONSE_TOKEN_GENERATOR)) {
+			return false;
+		}
+		
 		return true;
 	}
 
@@ -126,6 +187,37 @@ public class NAFIdentityPlugin implements IdentityPlugin {
 	        map.put(key, value);
 	    }
 	    return map;
+	}	
+	
+	protected String doGetRequest(String endpoint, String username, String password) {
+		HttpResponse response = null;
+		String responseStr = null;
+		try {
+			HttpUriRequest request = new HttpGet(endpoint);			
+			request.addHeader(NAME, username);
+			request.addHeader(PASSWORD, password);
+			response = client.execute(request);
+			responseStr = EntityUtils.toString(response.getEntity(), Charsets.UTF_8);
+		} catch (Exception e) {
+			LOGGER.error("Could not make GET request.", e);
+			throw new OCCIException(ErrorType.BAD_REQUEST, ResponseConstants.IRREGULAR_SYNTAX);
+		} finally {
+			try {
+				EntityUtils.consume(response.getEntity());
+			} catch (Throwable t) {
+				// Do nothing
+			}
+		}
+		checkStatusResponse(response, responseStr);
+		return responseStr;
+	}	
+	
+	protected void checkStatusResponse(HttpResponse response, String message) {
+		if (response.getStatusLine().getStatusCode() == HttpStatus.SC_UNAUTHORIZED) {
+			throw new OCCIException(ErrorType.UNAUTHORIZED, ResponseConstants.UNAUTHORIZED);
+		} else if (response.getStatusLine().getStatusCode() > 204) {
+			throw new OCCIException(ErrorType.BAD_REQUEST, response.getStatusLine().toString());
+		}
 	}	
 	
 	private class AccessIdFormat {
--- src/test/java/org/fogbowcloud/manager/core/plugins/identity/naf/TestNAFIdentityPlugin.java
+++ src/test/java/org/fogbowcloud/manager/core/plugins/identity/naf/TestNAFIdentityPlugin.java
@@ -1,28 +1,48 @@
 package org.fogbowcloud.manager.core.plugins.identity.naf;
 
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicStatusLine;
 import org.apache.xml.security.utils.Base64;
 import org.fogbowcloud.manager.core.plugins.util.Credential;
 import org.fogbowcloud.manager.occi.model.OCCIException;
+import org.fogbowcloud.manager.occi.model.OCCIHeaders;
 import org.fogbowcloud.manager.occi.model.Token;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
 
 public class TestNAFIdentityPlugin {
-
 	
 	private static final String DEFAULT_FILE_PUBLIC_KEY_PATH = "src/test/resources/public-key";
 	private NAFIdentityPlugin nafIdentityPlugin;
@@ -32,6 +52,7 @@ public class TestNAFIdentityPlugin {
 	private Map<String, String> defaultSamlAttributes;
 	private static final String ATTRIBUTE_ONE_SAML_ATTRIBUTE = "attributeOne";
 	private long defaultCTime;
+	private Properties properties;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -46,9 +67,9 @@ public class TestNAFIdentityPlugin {
 		this.defaultSamlAttributes.put(ATTRIBUTE_ONE_SAML_ATTRIBUTE, "valueAttributeOne");
 		
 		writePublicKeyToFile(RSAUtils.savePublicKey(this.keyPair.getPublic()));		
-		Properties properties = new Properties();
-		properties.put(NAFIdentityPlugin.NAF_PUBLIC_KEY, DEFAULT_FILE_PUBLIC_KEY_PATH);
-		this.nafIdentityPlugin = new NAFIdentityPlugin(properties);
+		this.properties = new Properties();
+		this.properties.put(NAFIdentityPlugin.NAF_PUBLIC_KEY, DEFAULT_FILE_PUBLIC_KEY_PATH);
+		this.nafIdentityPlugin = Mockito.spy(new NAFIdentityPlugin(this.properties));
 	}
 	
 	@After
@@ -63,6 +84,7 @@ public class TestNAFIdentityPlugin {
 	public void testIsValid() throws Exception {
 		Assert.assertTrue(this.nafIdentityPlugin.isValid(createAccessId()));
 	}
+
 	
 	@Test
 	public void testIsInvalid() throws Exception {
@@ -73,7 +95,79 @@ public class TestNAFIdentityPlugin {
 	public void testIsInvalidWithoutublicKey() throws Exception {
 		this.nafIdentityPlugin = new NAFIdentityPlugin(new Properties());
 		Assert.assertFalse(this.nafIdentityPlugin.isValid("wrong_token"));
+	}
+	
+	@Test
+	public void testIsValidWithTokenGenerator() throws Exception {
+		this.properties.put(NAFIdentityPlugin.ENDPOINT_TOKEN_GENERATOR, "https://localhost:0000");
+		this.properties.put(NAFIdentityPlugin.NAME_USER_TOKEN_GENERATOR, "name");
+		this.properties.put(NAFIdentityPlugin.PASSWORD_USER_TOKEN_GENERATOR, "password");
+		
+		HttpClient client = Mockito.mock(HttpClient.class);
+		HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
+		HttpEntity httpEntity = Mockito.mock(HttpEntity.class);
+		String content = NAFIdentityPlugin.VALID_RESPONSE_TOKEN_GENERATOR;
+		InputStream contentInputStream = new ByteArrayInputStream(content.getBytes(
+				StandardCharsets.UTF_8));;
+		Mockito.when(httpEntity.getContent()).thenReturn(contentInputStream);
+		Mockito.when(httpResponse.getEntity()).thenReturn(httpEntity);
+		BasicStatusLine basicStatus = new BasicStatusLine(new ProtocolVersion("", 0, 0),
+				HttpStatus.SC_OK, "");
+		Mockito.when(httpResponse.getStatusLine()).thenReturn(basicStatus);
+		Mockito.when(client.execute((Mockito.any(HttpUriRequest.class)))).thenReturn(
+				httpResponse);
+		
+		this.nafIdentityPlugin.setClient(client);
+		Assert.assertTrue(this.nafIdentityPlugin.isValid(createAccessIdTokenGenerator()));
+	}
+	
+	@Test
+	public void testIsInvalidWithTokenGenerator() throws Exception {
+		String url = "https://localhost:0000";
+		this.properties.put(NAFIdentityPlugin.ENDPOINT_TOKEN_GENERATOR, url);
+		String name = "name";
+		this.properties.put(NAFIdentityPlugin.NAME_USER_TOKEN_GENERATOR, name);
+		String password = "password";
+		this.properties.put(NAFIdentityPlugin.PASSWORD_USER_TOKEN_GENERATOR, password);
+		
+		HttpClient client = Mockito.mock(HttpClient.class);
+		HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
+		HttpEntity httpEntity = Mockito.mock(HttpEntity.class);
+		String content = "Invalid";
+		InputStream contentInputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));;
+		Mockito.when(httpEntity.getContent()).thenReturn(contentInputStream);
+		Mockito.when(httpResponse.getEntity()).thenReturn(httpEntity);
+		BasicStatusLine basicStatus = new BasicStatusLine(new ProtocolVersion("", 0, 0), HttpStatus.SC_OK, "");
+		Mockito.when(httpResponse.getStatusLine()).thenReturn(basicStatus);
+		Mockito.when(client.execute((Mockito.any(HttpUriRequest.class)))).thenReturn(httpResponse);
+				
+		this.nafIdentityPlugin.setClient(client);
+				
+		String accessIdTokenGenerator = createAccessIdTokenGenerator();
+		Assert.assertFalse(this.nafIdentityPlugin.isValid(accessIdTokenGenerator));	
 	}	
+	
+	@Test
+	public void testCheckUrlTokenGenerator() throws Exception {
+		String url = "https://localhost:0000";
+		this.properties.put(NAFIdentityPlugin.ENDPOINT_TOKEN_GENERATOR, url);
+		String name = "name";
+		this.properties.put(NAFIdentityPlugin.NAME_USER_TOKEN_GENERATOR, name);
+		String password = "password";
+		this.properties.put(NAFIdentityPlugin.PASSWORD_USER_TOKEN_GENERATOR, password);
+		
+		String accessIdTokenGenerator = createAccessIdTokenGenerator();
+		HttpUriRequest request = new HttpGet(url + NAFIdentityPlugin.TOKEN_URL_OPERATION 
+				+ URLEncoder.encode(accessIdTokenGenerator, "UTF-8") + NAFIdentityPlugin.METHOD_GET_VALIDITY_CHECK);
+		request.addHeader(NAFIdentityPlugin.NAME, name);
+		request.addHeader(NAFIdentityPlugin.PASSWORD, password);
+		HttpUriRequestMatcher expectedRequest = new HttpUriRequestMatcher(request);
+		
+		this.nafIdentityPlugin.setClient(Mockito.spy(HttpClients.createMinimal()));
+		Assert.assertFalse(this.nafIdentityPlugin.isValid(accessIdTokenGenerator));
+		Mockito.verify(this.nafIdentityPlugin.getClient(), Mockito.times(1)).
+				execute(Mockito.argThat(expectedRequest));
+	}
 	
 	@Test
 	public void testGetToken() throws Exception {
@@ -120,17 +214,65 @@ public class TestNAFIdentityPlugin {
 		Assert.assertEquals(originalToken, this.nafIdentityPlugin.reIssueToken(originalToken));		
 	}
 	
+	@Test
+	public void testCheckStatusResponseUnauthorized() {
+		HttpResponse response = Mockito.mock(HttpResponse.class);
+		int httpStatusCode = HttpStatus.SC_UNAUTHORIZED;
+		StatusLine statusLine = new BasicStatusLine(new ProtocolVersion("", 0, 0), 
+				httpStatusCode, "");
+		Mockito.when(response.getStatusLine()).thenReturn(statusLine);
+		try {
+			this.nafIdentityPlugin.checkStatusResponse(response, "");			
+		} catch (OCCIException e) {
+			Assert.assertEquals(httpStatusCode, e.getStatus().getCode());
+			return;
+		}
+		Assert.fail();
+	}
+	
+	@Test
+	public void testCheckStatusResponseBadRequest() {
+		HttpResponse response = Mockito.mock(HttpResponse.class);
+		int httpStatusCode = HttpStatus.SC_BAD_REQUEST;
+		StatusLine statusLine = new BasicStatusLine(new ProtocolVersion("", 0, 0), 
+				httpStatusCode, "");
+		Mockito.when(response.getStatusLine()).thenReturn(statusLine);
+		try {
+			this.nafIdentityPlugin.checkStatusResponse(response, "");			
+		} catch (OCCIException e) {
+			Assert.assertEquals(httpStatusCode, e.getStatus().getCode());
+			return;
+		}
+		Assert.fail();
+	}	
+	
 	private String createAccessId() throws Exception {
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put(NAFIdentityPlugin.NAME, DEFAULT_USER);
-		jsonObject.put(NAFIdentityPlugin.TOKEN_CTIME_JSONOBJECT, this.defaultCTime);		
+		jsonObject.put(NAFIdentityPlugin.TOKEN_ETIME_JSONOBJECT, this.defaultCTime);		
 		jsonObject.put(NAFIdentityPlugin.SAML_ATTRIBUTES_JSONOBJECT, this.defaultSamlAttributes);
 		String message = jsonObject.toString();
 		
+		return createFinalToken(message);
+	}
+	
+	private String createAccessIdTokenGenerator() throws Exception {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put(NAFIdentityPlugin.NAME, DEFAULT_USER);
+		jsonObject.put(NAFIdentityPlugin.TOKEN_ETIME_JSONOBJECT, this.defaultCTime);		
+		jsonObject.put(NAFIdentityPlugin.TYPE, NAFIdentityPlugin.DEFAULT_TYPE_TOKEN_GENERATOR);
+		String message = jsonObject.toString();
+		
+		return createFinalToken(message);
+	}
+
+	private String createFinalToken(String message) throws NoSuchAlgorithmException,
+			InvalidKeyException, SignatureException,
+			UnsupportedEncodingException {
 		String signature = RSAUtils.sign(keyPair.getPrivate(), message);
 		String accessIdStr = message + NAFIdentityPlugin.STRING_SEPARATOR + signature;
 		return Base64.encode(accessIdStr.getBytes());
-	}
+	}	
 	
 	public void writePublicKeyToFile(String content) throws IOException {
 		File file = new File(DEFAULT_FILE_PUBLIC_KEY_PATH);
@@ -143,6 +285,53 @@ public class TestNAFIdentityPlugin {
 		BufferedWriter bw = new BufferedWriter(fw);
 		bw.write(content);
 		bw.close();
+	}	
+	
+	private class HttpUriRequestMatcher extends ArgumentMatcher<HttpUriRequest> {
+
+		private HttpUriRequest request;
+
+		public HttpUriRequestMatcher(HttpUriRequest request) {
+			this.request = request;
+		}
+
+		public boolean matches(Object object) {
+
+			HttpUriRequest comparedRequest = (HttpUriRequest) object;
+			if (!this.request.getURI().equals(comparedRequest.getURI())) {
+				return false;
+			}
+			if (!checkHeaders(comparedRequest.getAllHeaders())) {
+				return false;
+			}
+			if (!this.request.getMethod().equals(comparedRequest.getMethod())) {
+				return false;
+			}
+			return true;
+		}
+
+		public boolean checkHeaders(Header[] comparedHeaders) {
+			for (Header comparedHeader : comparedHeaders) {
+				boolean headerEquals = false;
+				for (Header header : this.request.getAllHeaders()) {
+					if (header.getName().equals(OCCIHeaders.X_AUTH_TOKEN)) {
+						if (header.getName().equals(comparedHeader.getName())) {
+							headerEquals = true;
+							break;
+						}
+					} else 
+					if (header.getName().equals(comparedHeader.getName())
+							&& header.getValue().equals(comparedHeader.getValue())) {
+						headerEquals = true;
+						continue;
+					}
+				}
+				if (!headerEquals) {
+					return false;
+				}
+			}
+			return true;
+		}
 	}	
 	
 }
