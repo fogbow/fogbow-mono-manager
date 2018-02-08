28caf4b2b6a84ec77a3b6f4d09e7713909ea6fc4
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-02-08 17:27:32 -0300

    Added error messages and public constantes to OCCIExceptions

--- src/main/java/org/fogbowcloud/manager/occi/federatednetwork/FederatedNetworkConstants.java
+++ src/main/java/org/fogbowcloud/manager/occi/federatednetwork/FederatedNetworkConstants.java
@@ -5,5 +5,8 @@ public class FederatedNetworkConstants {
 	public static final String FEDERATED_NETWORK_TERM = "federatedNetwork";
 	
 	public static final String FEDERATED_NETWORK_ID_TERM = "federatedNetworkId";
+	
+	public static final String NOT_FOUND_FEDERATED_NETWORK_MESSAGE = "Not found Federated Network with Id: ";
+	public static final String NOT_FOUND_PUBLIC_AGENT_IP_MESSAGE = "Not found Public Agent IP in Configuration File";
 
 }
