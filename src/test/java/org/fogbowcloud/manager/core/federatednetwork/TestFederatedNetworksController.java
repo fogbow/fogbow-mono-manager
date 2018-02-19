1665b3abaf7d03b79c1bf86ee1d109c26e99efe3
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-02-19 19:40:00 -0300

    Implementing mocked tests for removing federated networks on agent

--- src/test/java/org/fogbowcloud/manager/core/federatednetwork/TestFederatedNetworksController.java
+++ src/test/java/org/fogbowcloud/manager/core/federatednetwork/TestFederatedNetworksController.java
@@ -45,7 +45,7 @@ public class TestFederatedNetworksController {
 
 		FederatedNetwork fn = controller.getFederatedNetwork(user, federatedNetworkId);
 		Assert.assertEquals(2, fn.getAllowedMembers().size());
-		
+
 		String ipOne = controller.getPrivateIpFromFederatedNetwork(user, federatedNetworkId,
 				"fake-orderId");
 		String ipTwo = controller.getPrivateIpFromFederatedNetwork(user, federatedNetworkId,
@@ -53,10 +53,58 @@ public class TestFederatedNetworksController {
 
 		fn = controller.getFederatedNetwork(user, federatedNetworkId);
 		Assert.assertNotEquals(ipOne, ipTwo);
-		
+
 		String ipThree = controller.getPrivateIpFromFederatedNetwork(user, federatedNetworkId,
 				"fake-orderId3");
 		Assert.assertNotEquals(ipOne, ipThree);
+	}
+
+	@Test
+	public void testRemoveFederatedNetwork() throws SubnetAddressesCapacityReachedException {
+		Properties properties = null;
+		FederatedNetworksController controller = Mockito
+				.spy(new FederatedNetworksController(properties, DATABASE_FILE_PATH));
+		Mockito.doReturn(true).when(controller).callFederatedNetworkAgent(Mockito.anyString(),
+				Mockito.anyString());
+		Mockito.doReturn(true).when(controller).removeFederatedNetworkAgent(Mockito.anyString());
+
+		Token.User user = new Token.User("hardCodedId", "hardCodedName");
+		String label = "hardCodedLabel";
+		String cidrNotation = "10.0.0.0/24";
+		HashSet<FederationMember> members = new HashSet<>();
+
+		String federatedNetworkId = controller.create(user, label, cidrNotation, members);
+		Assert.assertNotNull(federatedNetworkId);
+		Assert.assertEquals(1, controller.getUserNetworks(user).size());
+
+		controller.deleteFederatedNetwork(user, federatedNetworkId);
+		Assert.assertEquals(0, controller.getUserNetworks(user).size());
+	}
+
+	@Test
+	public void testRemoveFederatedNetworkWhenGetScriptError() throws SubnetAddressesCapacityReachedException {
+		Properties properties = null;
+		FederatedNetworksController controller = Mockito
+				.spy(new FederatedNetworksController(properties, DATABASE_FILE_PATH));
+		Mockito.doReturn(true).when(controller).callFederatedNetworkAgent(Mockito.anyString(),
+				Mockito.anyString());
+		Mockito.doReturn(false).when(controller).removeFederatedNetworkAgent(Mockito.anyString());
+
+		Token.User user = new Token.User("hardCodedId", "hardCodedName");
+		String label = "hardCodedLabel";
+		String cidrNotation = "10.0.0.0/24";
+		HashSet<FederationMember> members = new HashSet<>();
+
+		String federatedNetworkId = controller.create(user, label, cidrNotation, members);
+		Assert.assertNotNull(federatedNetworkId);
+		Assert.assertEquals(1, controller.getUserNetworks(user).size());
+
+		controller.deleteFederatedNetwork(user, federatedNetworkId);
+		Assert.assertEquals(1, controller.getUserNetworks(user).size());
+
+		Mockito.doReturn(true).when(controller).removeFederatedNetworkAgent(Mockito.anyString());
+		controller.deleteFederatedNetwork(user, federatedNetworkId);
+		Assert.assertEquals(0, controller.getUserNetworks(user).size());
 	}
 
 }
