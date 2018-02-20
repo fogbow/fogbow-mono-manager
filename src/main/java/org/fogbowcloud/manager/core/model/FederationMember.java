1d1cc08cc8299c049436ad485f53d143acbd4893
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-02-20 14:11:09 -0300

    Added hashCode method and update equals method

--- src/main/java/org/fogbowcloud/manager/core/model/FederationMember.java
+++ src/main/java/org/fogbowcloud/manager/core/model/FederationMember.java
@@ -57,12 +57,28 @@ public class FederationMember {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof FederationMember)) {
+		if (this == obj)
+			return true;
+		if (obj == null)
 			return false;
-		}
-		FederationMember otherMember = (FederationMember) obj;
-		return getId().equals(otherMember.getId());
+		if (getClass() != obj.getClass())
+			return false;
+		FederationMember other = (FederationMember) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	@Override
